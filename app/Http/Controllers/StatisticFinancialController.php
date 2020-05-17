4fb7be0e9b3ec887a3e343ba2208c26ad7587ae4
HFrada <madefrada@gmail.com>
2020-05-17 12:09:11 +0700

    fixing Ibnu kmprt

--- app/Http/Controllers/StatisticFinancialController.php
+++ app/Http/Controllers/StatisticFinancialController.php
@@ -10,10 +10,12 @@ class StatisticFinancialController extends Controller
 {
 	public function index()
 	{
-		return Financial::select(['users.name as username', 'financial.type', 'financial.amount', DB::raw('COUNT(user_id) as item_count')])
-			->join('users', 'financial.user_id', '=', 'users.id')
-			->groupBy('username')
-			->havingRaw('COUNT(user_id) >= 1')
-			->get();
+		return Financial::join('users', 'financial.user_id', '=', 'users.id')
+		->select(['users.name as username', 'financial.type', 'financial.amount'])
+		->addSelect(DB::raw('SUM(financial.amount) as total_amount'))
+		->addSelect(DB::raw('SUM(IF(financial.type = "pay", financial.amount, 0)) as pay'))
+		->addSelect(DB::raw('SUM(IF(financial.type = "wallet", financial.amount, 0)) as wallet'))
+		->groupBy('username')
+		->get();
 	}
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,9 @@ class DatabaseSeeder extends Seeder
     {
     	factory(App\User::class, 10)->create();
 
-        $this->call(CategorySeeder::class);
-        $this->call(FinancialSeeder::class);
+        $this->call([
+            CategorySeeder::class,
+            FinancialSeeder::class
+        ]);
     }
 }
