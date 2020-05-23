4d03182aa9d23f602128ce98353d61f7c18f8f24
IbnuSatoRaiga <penggemarpb40@gmail.com>
2020-05-23 17:52:04 +0700

    Add select option category for form financial

---
+++ resources/js/components/SelectCategoryComponent.vue
@@ -0,0 +1,33 @@
+<template>
+	<v-select 
+		label="ID for Category"
+		item-text="category" 
+		item-value="id" 
+		v-model="value"
+		:items="items"
+		required>
+	</v-select>
+</template>
+<script>
+	
+	import dataProcess from '../mixin/dataProcess.js';
+
+	export default
+	{
+		props : ['value'],
+		mixins : [dataProcess],
+
+		data()
+		{
+			return {
+				url : '/api/v1/category?sort_by=id'
+			}
+		},
+
+		watch:{
+			value(val){
+				this.$emit('input', val)
+			}
+		},
+	}
+</script>
